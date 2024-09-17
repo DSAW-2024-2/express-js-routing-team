24e5cdde39dff25acd21c965de755e1a66baed4b
gabrielabejarano09 <gabrielabejarano09@gmail.com>
2024-09-16 22:04:31 -0500

    add get and delete for user

--- Routes/user.js
+++ Routes/user.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send({ data: 'User route' });
+const users = [
+    { id: 1, name: 'Alexander Gomez', email: 'alexanderg@gmail.com', age: 30 },
+    { id: 2, name: 'Tomas Pérez', email: 'tomas.pe@gmail.com', age: 25 },
+    { id: 3, name: 'Carlos Pinzon', email: 'pinzon.carlos@gmail.com', age: 40 },
+    { id: 4, name: 'John Silva', email: 'j.silvas@gmail.com', age: 76 },
+    { id: 5, name: 'Ximena Rodiguez', email: 'ximenarodriguez89@gmail.com', age: 32 }
+  ];
+
+router.get('/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const user = users.find(u => u.id === userId); 
+  
+    if (user) {
+      res.status(200).json({ data: user }); 
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
 });
 
 router.post('/', (req, res) => {
@@ -13,6 +28,14 @@ router.put('/', (req, res) => {
   res.send({ data: 'User route' });
 });
 
-router.delete('/', (req, res) => {
-  res.send({ data: 'User route' });
-});
+router.delete('/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const user = users.find(u => u.id === userId); 
+  
+    if (user) {
+        users = users.filter(u => u.id !== userId);
+        res.status(200).json({ message: 'Deleted User', data: user });
+    } else {
+      res.status(404).json({ error: 'User was not found' });
+    }
+  });
