e22c4aeab8784056da1de3861678a41494dbbc2c
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-09-17 17:26:46 -0500

    Some little changes were made so that the program could run whithout problems

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 userRoute = require('./Routes/user');
 productRoute = require('./Routes/product');
 
